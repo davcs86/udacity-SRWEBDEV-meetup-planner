96371ca50822ce8a1262c3d5190be9e527c53153
David Castillo <davcs86@gmail.com>
2016-08-23 11:22:49 -0400

    Codebase generated with Yeoman's generator-gulp-angular

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('udacitySrwebdevMeetupPlanner', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
